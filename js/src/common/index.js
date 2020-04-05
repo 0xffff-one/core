99644ac8729edec245fe28395cbba192333fdfce
zgq354 <zgq354@gmail.com>
2020-04-05 22:23:31 +0800

    fix: moment utc support

--- js/src/common/index.js
+++ js/src/common/index.js
@@ -13,9 +13,11 @@ import patchMithril from './utils/patchMithril';
 
 import relativeTime from 'dayjs/plugin/relativeTime';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
+dayjs.extend(utc);
 
 patchMithril(window);
 
